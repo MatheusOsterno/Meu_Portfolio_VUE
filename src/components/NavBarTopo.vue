95e5f7c20e17b72ca16b304a033f6912ea5e74fb
MatheusOsterno <matheusosternodev@gmail.com>
2022-10-06 23:03:41 -0300

    Primeiro

---
+++ src/components/NavBarTopo.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar app color="#111111" dark flat class="px-8">
+    <v-btn icon>
+      <v-icon color="#a71210">fas fa-meteor</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll(home)">Home</v-btn>
+    <v-btn text @click="scroll(projetos)">Projetos</v-btn>
+    <v-btn text @click="scroll(sobre)">Sobre</v-btn>
+    <v-btn text @click="scroll(Contato)">Contato</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({behavior: "smooth"});
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+<v-app id="home" :style="{ background: $vuetify.theme.themes.dark.background}">
+<NavBarTopo />
+<v-container fluid>
+  <v-row>
+    <v-col cols="6">
+      <v-img src="Matheus.png" contain max-height="500"></v-img>
+    </v-col>
+    <v-col cols="6">
+      <h5 class="red--text text-darken-4 top">Olá, me chamo Matheus Osterno</h5>
+      <h1 class="white--text">Sou desenvolvedor Front-End</h1>
+      <p class="grey--text">
+        Nesse portfólio mostro alguns de meus projetos.
+      </p>
+      <v-btn tile color="#A71210" dark>Baixe meu curriculo</v-btn>
+    </v-col>
+  </v-row>
+</v-container>
+
+</v-app>
+</template>
+
+<script>
+
+import NavBarTopo from '../components/NavBarTopo.vue'
+
+  export default {
+    components: {
+    NavBarTopo
+},
+  }
+</script>
+
+<style scoped>
+.top{
+  margin-top: 180px;
+}
+</style>
